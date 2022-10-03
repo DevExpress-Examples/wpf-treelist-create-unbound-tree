c210c0d5d74b66c17e9f7132101a7460b255297c
DevExpressExampleBot <fakeEmail>
2022-10-03 13:46:46 +0400

    Source auto update [skip ci]

--- VB/TreeListView_UnboundMode/MainWindow.xaml.vb
+++ VB/TreeListView_UnboundMode/MainWindow.xaml.vb
@@ -1,62 +1,50 @@
-﻿Imports System
 Imports System.Windows
 Imports DevExpress.Xpf.Grid
 
 Namespace TreeListView_UnboundMode
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-			BuildTree()
-			treeListView1.ExpandAllNodes()
-		End Sub
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Private Sub BuildTree()
-			Dim rootNode As TreeListNode = CreateRootNode(New ProjectObject() With {
-				.Name = "Project: Stanton",
-				.Executor = "Nicholas Llams"
-			})
-			Dim childNode As TreeListNode = CreateChildNode(rootNode, New StageObject() With {
-				.Name = "Information Gathering",
-				.Executor = "Ankie Galva"
-			})
-			CreateChildNode(childNode, New TaskObject() With {
-				.Name = "Design",
-				.Executor = "Reardon Felton",
-				.State = "In progress"
-			})
-		End Sub
+        Public Sub New()
+            Me.InitializeComponent()
+        'BuildTree();
+        End Sub
 
-		Private Function CreateRootNode(ByVal dataObject As Object) As TreeListNode
-			Dim rootNode As New TreeListNode(dataObject)
-			treeListView1.Nodes.Add(rootNode)
-			Return rootNode
-		End Function
+        Private Sub BuildTree()
+            Dim rootNode As TreeListNode = CreateRootNode(New ProjectObject() With {.Name = "Project: Stanton", .Executor = "Nicholas Llams"})
+            Dim childNode As TreeListNode = CreateChildNode(rootNode, New StageObject() With {.Name = "Information Gathering", .Executor = "Ankie Galva"})
+            CreateChildNode(childNode, New TaskObject() With {.Name = "Design", .Executor = "Reardon Felton", .State = "In progress"})
+        End Sub
 
-		Private Function CreateChildNode(ByVal parentNode As TreeListNode, ByVal dataObject As Object) As TreeListNode
-			Dim childNode As New TreeListNode(dataObject)
-			parentNode.Nodes.Add(childNode)
-			Return childNode
-		End Function
-	End Class
+        Private Function CreateRootNode(ByVal dataObject As Object) As TreeListNode
+            Dim rootNode As TreeListNode = New TreeListNode(dataObject)
+            Me.view.Nodes.Add(rootNode)
+            Return rootNode
+        End Function
 
-	Public Class StageObject
-		Public Property Name() As String
-		Public Property Executor() As String
-	End Class
+        Private Function CreateChildNode(ByVal parentNode As TreeListNode, ByVal dataObject As Object) As TreeListNode
+            Dim childNode As TreeListNode = New TreeListNode(dataObject)
+            parentNode.Nodes.Add(childNode)
+            Return childNode
+        End Function
+    End Class
 
-	Public Class ProjectObject
-		Public Property Name() As String
-		Public Property Executor() As String
-	End Class
+    Public Class ProjectObject
 
-	Public Class TaskObject
-		Public Property Name() As String
-		Public Property Executor() As String
-		Public Property State() As String
-	End Class
+        Public Property Name As String
+
+        Public Property Executor As String
+    End Class
+
+    Public Class StageObject
+        Inherits ProjectObject
+
+    End Class
+
+    Public Class TaskObject
+        Inherits ProjectObject
+
+        Public Property State As String
+    End Class
 End Namespace
